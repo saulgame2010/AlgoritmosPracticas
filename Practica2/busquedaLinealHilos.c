89a19f6ae9a43fb68dd86c0f0177ecf6861a2f4f
saulgame2010 <35746616+saulgame2010@users.noreply.github.com>
2020-11-18 12:43:51 -0600

    v1.0.2
    caca

--- Practica2/busquedaLinealHilos.c
+++ Practica2/busquedaLinealHilos.c
@@ -1,3 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+
+void *procesoSeparado(void *data) {
+	char *texto = (char *) data;
+	while(1) {
+		printf("%s\n", texto);
+	}
+}
+
+int main(void)
+{
+	pthread_t proceso1;
+	pthread_t proceso2;
+	pthread_create(&proceso1, NULL, &procesoSeparado, "hola");
+	pthread_create(&proceso2, NULL, &procesoSeparado, "adios");
+	pthread_join(proceso1, NULL);
+	pthread_join(proceso2, NULL);
+	return 0;
+}
